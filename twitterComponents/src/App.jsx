ed6b96379126f16e8191b20837ba41d8bbfb9c9f
tsoiffer <tsoiffer@frba.utn.edu.ar>
2023-02-23 23:00:44 -0300

    le sumo fetching de datos al componente TwitterFollowCard

--- twitterComponents/src/App.jsx
+++ twitterComponents/src/App.jsx
@@ -1,27 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 import "./App.css";
 
 function App() {
-  const users = [
-    {imgLink: "https://pbs.twimg.com/profile_images/1282207374707761152/FipRN1k7_400x400.jpg" ,name: "TomasSoiffer", userName: "@tomassoiffer", initialIsFollowing : true },
-    {imgLink: "https://pbs.twimg.com/profile_images/1282207374707761152/FipRN1k7_400x400.jpg" ,name: "TomasSoiffer", userName: "@tomassoiffer", initialIsFollowing : false },
-    {imgLink: "https://pbs.twimg.com/profile_images/1282207374707761152/FipRN1k7_400x400.jpg" ,name: "TomasSoiffer", userName: "@tomassoiffer", initialIsFollowing : true }
-  ]
+
+  let users = {results :
+   [
+    {picture: { medium:"https://pbs.twimg.com/profile_images/1282207374707761152/FipRN1k7_400x400.jpg"} ,name: {first:"TomasSoiffer"}, email: "@tomassoiffer", initialIsFollowing : true },
+    {picture: { medium:"https://pbs.twimg.com/profile_images/1282207374707761152/FipRN1k7_400x400.jpg"} ,name: {first:"TomasSoiffer"}, email: "@tomassoiffer", initialIsFollowing : true },
+    {picture: { medium:"https://pbs.twimg.com/profile_images/1282207374707761152/FipRN1k7_400x400.jpg"} ,name: {first:"TomasSoiffer"}, email: "@tomassoiffer", initialIsFollowing : true }
+  ]}
+  const [users2,setUser] = useState(users)
+  
+    useEffect( () => {fetch('https://randomuser.me/api/?results=5')
+    .then(result=>result.json())
+    .then(items=>setUser(items))
+    .catch(() => console.log("Algo falló"))
+    },[]
+    )
+    console.log(users2.results)
+      
+  
+
     return (
     <div className="whoFollow">
       <h3>A quién seguir</h3>
       {
-        users.map(({imgLink,name,userName,initialIsFollowing}) =>  (
+        users2.results.map(({picture,name,email}) =>  (
         <TwitterFollowCard
-       imgLink = {imgLink}
-       name = {name}
-       userName = {userName}
-       initialIsFollowing = {initialIsFollowing}
+       imgLink = {picture.medium}
+       name = {name.first}
+       userName = {email}
+       initialIsFollowing = {false}
        />
        )
         )
+        
       }
       
     </div>
